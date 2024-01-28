f9703300b896b0e6ff7e7d8f06b3c976d3f8cb2a
songseungeun <grayandjj@gmail.com>
2024-01-29 01:11:21 +0900

    feat: prettier 설정 추가

--- src/controllers/reservation.controller.spec.ts
+++ src/controllers/reservation.controller.spec.ts
@@ -1,7 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ReservationController } from './reservation.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { ReservationController } from "./reservation.controller";
 
-describe('ReservationController', () => {
+describe("ReservationController", () => {
   let controller: ReservationController;
 
   beforeEach(async () => {
@@ -12,7 +12,7 @@ describe('ReservationController', () => {
     controller = module.get<ReservationController>(ReservationController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
