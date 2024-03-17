08eed597c41f6baef8bf0e062f8fe12c58ae7cd7
songseungeun <grayandjj@gmail.com>
2024-03-18 02:00:01 +0900

    fix: typeorm config 추가, user entity 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ReservationController } from "./reservation/reservation.controller";
 import { AuthModule } from "./auth/auth.module";
 import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "./config/config.module";
@@ -9,7 +8,7 @@ import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [ConfigModule, DatabaseModule, AuthModule, UserModule],
-  controllers: [AppController, ReservationController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
